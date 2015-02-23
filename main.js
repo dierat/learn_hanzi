0639b55540131706e2bf162a5ffe902a801bdd99
dierat <diedra.rater@gmail.com>
2015-02-22 18:42:11 -0800

    removed the startup function that fills the deck; made it into a Meteor method along with emptying the Collections so it can be done with a single method. also fixed the bug where it would show a Waiting card sometimes when there was a Current card ready

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ Current_deck = new Mongo.Collection("current_deck");
 Waiting_deck = new Mongo.Collection("waiting_deck");
 
 
+var chars = [['一','one'],['亠','lid'],['冖','cover'],['宀','roof'],
+  ['立','stand, erect'], ['厂','cliff'], ['广','house on cliff'], 
+  ['疒','sickness'], ['穴','cave'], ['人','human, person, people']];
+
+
+
 time_levels = [15.0, 30.0, 60.0, 120.0, 240.0, 480.0, 960.0, 1920.0, 3840.0,
 7680.0, 15360.0, 30720.0, 61440.0, 122880.0, 245760.0, 491520.0, 983040.0, 
 1966080.0, 3932160.0, 7864320.0]
@@ -18,10 +24,14 @@ if (Meteor.isClient) {
     'disable_warning': 'false'
   });
 
+  Meteor.startup(function() {
+    Session.setDefault("date", new Date());
+  })
+
   Template.body.helpers({
     cards: function () {
-      var current_card = Current_deck.find({time: {$lt: new Date()}}, {sort: {time: 1}, limit: 1});
-      if (current_card.count()) {
+      var current_card = Current_deck.find({time: {$lt: Session.get("date")}}, {sort: {time: 1}, limit: 1});
+      if (current_card.count() > 0) {
         return current_card;
       } else {
         return Waiting_deck.find({}, {sort: {time: 1}, limit: 1});
@@ -31,13 +41,14 @@ if (Meteor.isClient) {
 
   Template.card.events({
     'click #next-button': function () {
+      Session.set('date', new Date());
       this.seen = true;
-      this.time = new Date(+new Date() + time_levels[this.level]*1000);
+      this.time = new Date(+new Date() + time_levels[0]*1000);
       Current_deck.insert(this);
       Waiting_deck.remove(this._id);
     },
-    'click #answer-button': function () {
-      var answer = $("#answer-field").val();
+    'submit .answer': function (event) {
+      var answer = event.target.text.value;
       if (answer === this.meaning) {
         console.log("Correct!!");
         console.log("The current time is: " + new Date())
@@ -46,9 +57,15 @@ if (Meteor.isClient) {
         console.log("Nope! It's " + this.meaning);
         console.log("The current time is: " + new Date())
         Current_deck.update(this._id, {$set: {time: new Date(+new Date() + time_levels[this.level]*1000)}});
+        event.target.text.value = '';
       }
+      return false;
     }
   });
+
+  Template.card.rendered = function () {
+    $('.answer input').focus();
+  }
 
 }
 
@@ -56,22 +73,31 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 
-  Meteor.startup(function () {
-    if (Waiting_deck.find().count() === 0) {
-      var chars = [['一','one'],['亠','lid'],['冖','cover'],['宀','roof'],
-      ['立','stand, erect'], ['厂','cliff'], ['广','house on cliff'], 
-      ['疒','sickness'], ['穴','cave'], ['人','human, person, people'], ];
-
-      _.each(chars, function (char) {
-        Waiting_deck.insert({
-          character: char[0],
-          level: 0,
-          meaning: char[1],
-          time: new Date(),
-          seen: false
+  Meteor.methods({
+    fill_deck: function () {
+      if (Waiting_deck.find().count() === 0) {
+        _.each(chars, function (char) {
+          Waiting_deck.insert({
+            character: char[0],
+            meaning: char[1],
+            time: new Date(),
+            seen: false,
+            level: 0
+          });
         });
-      });
+      }
+    },
+
+    empty_deck: function() {
+      Current_deck.remove({});
+      Waiting_deck.remove({});
+    },
+
+    shuffle_deck: function() {
+      Meteor.call('empty_deck');
+      Meteor.call('fill_deck');
     }
+
   });
 
 }
