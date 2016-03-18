9d8165243ff86c920cbd0c97229faebb30826e39
dierat <diedra.rater@gmail.com>
2016-03-18 10:13:45 -0700

    comments out methods for resetting the database

--- app/server/meteorMethods.js
+++ app/server/meteorMethods.js
@@ -1,12 +1,24 @@
+// These methods allow you to easily reset the database from the browser console.
+// Uncomment the code in this file to use these commands, but recomment them out
+// before deployment so users can't reset your database from their browser.
+
+
+// The first method fills the Main_deck with the cards from the 'chars' array
+// in the chars.js file.
+// To call, type Meteor.call('fill_deck'); in the browser console.
+
+// The second method empties both decks.
+// To call, type Meteor.call('empty_deck'); in the browser console.
+
+// The third method calls both previous methods to empty the decks and then
+// fills the Main_deck with the cards from the 'chars' array in the chars.js
+// file.
+// To call, type Meteor.call('shuffle_deck'); in the browser console.
+
+
+/*
 Meteor.methods({
 
-  // These methods allow you to easily reset the database from the browser console
-  // (open it by holding down 'alt', 'cmd', and 'J').
-
-
-  // The first method fills the Main_deck with the cards from the 'chars' array
-  // in the chars.js file.
-  // To call, type Meteor.call('fill_deck'); in the browser console.
   fill_deck: function () {
     if (Main_deck.find().count() === 0) {
       _.each(chars, (char)=> {
@@ -22,20 +34,15 @@ Meteor.methods({
     }
   },
 
-  // The second method empties both decks.
-  // To call, type Meteor.call('empty_deck'); in the browser console.
   empty_deck: ()=> {
     Main_deck.remove({});
     Users_deck.remove({});
   },
 
-  // The third method calls both previous methods to empty the decks and then
-  // fills the Main_deck with the cards from the 'chars' array in the chars.js
-  // file.
-  // To call, type Meteor.call('shuffle_deck'); in the browser console.
   shuffle_deck: ()=> {
     Meteor.call('empty_deck');
     Meteor.call('fill_deck');
   }
 
 });
+*/
